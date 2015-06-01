cf37c99b4f9eeed36aaa378639e81d30f8f3e85e
connection-lost <frankwu1995@hotmail.com>
2015-06-01 16:43:33 -0400

    Added visit (webpage) function

--- app/src/main/java/me/crafter/android/zjsnviewer/Worker.java
+++ app/src/main/java/me/crafter/android/zjsnviewer/Worker.java
@@ -7,6 +7,11 @@ import android.util.TypedValue;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Worker {
 
     public static void widgetSetTextSize(Context context, RemoteViews views){
@@ -42,10 +47,24 @@ public class Worker {
         }.execute();
     }
 
-    public static void visit(String urls){
-
-
-
+    public static String visit(String urls){
+        String ret = "";
+        try {
+            URL url = new URL(urls);
+            URLConnection connection = url.openConnection();
+            connection.setConnectTimeout(15000);
+            connection.setReadTimeout(15000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            ret = response.toString();
+        } catch (Exception ex){
+            ret = "ERR1";
+        }
+        return ret;
     }
 
 }
