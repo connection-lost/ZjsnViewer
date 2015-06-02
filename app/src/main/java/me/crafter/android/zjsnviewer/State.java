be61b5f5d235581fd758c17e98d670ef6eedf86d
connection-lost <frankwu1995@hotmail.com>
2015-06-02 13:27:43 -0400

    Added a ENUM class - State

---
+++ app/src/main/java/me/crafter/android/zjsnviewer/State.java
@@ -0,0 +1,8 @@
+package me.crafter.android.zjsnviewer;
+
+/**
+ * Created by Frank on 2015/6/2.
+ */
+public enum State {
+    DEFAULT, UNKNOWN, INTERRUPT, CONNECTION_FAIL, NO_INTERNET, CONNECTION_RESET, PARSE_ERROR, NO_UPDATE_FOUND, UPDATE_FOUND;
+}
