a1a46563f1b9dd9360474312520d8082ff1a903e
traburiss <marlycool@qq.com>
2016-06-03 14:52:48 +0800

    颜色怎么搞都不好看啊~~

--- app/src/main/java/me/crafter/android/zjsnviewer/webactivity.java
+++ app/src/main/java/me/crafter/android/zjsnviewer/webactivity.java
@@ -2,7 +2,8 @@ package me.crafter.android.zjsnviewer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -13,12 +14,21 @@ public class Webactivity extends Activity{
 
     @BindView(R.id.web_view)
     WebView webView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_web);
         ButterKnife.bind(this);
+        toolbar.setTitle("web");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         String url = getIntent().getStringExtra("URL");
         WebSettings webSettings = webView.getSettings();
         webSettings.setSaveFormData(false);
